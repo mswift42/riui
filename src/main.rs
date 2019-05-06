e52c85795b7abef854e0bf3707146815a728adf7
mswift42 <martin.haesler@gmail.com>
2019-05-06 14:26:20 +0100

    add load_file method.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+use std::io::Read;
+
+fn load_file(path: &str) -> String {
+    let mut file = fs::File::open(&path).unwrap();
+    let mut text = String::new();
+    file.read_to_string(&mut text).unwrap();
+    text
+}
+
 fn main() {
     println!("Hello, world!");
 }
